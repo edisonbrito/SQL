15fdea0ef7f5832299b6ca4307c9c69a18a745ae
Edison Brito <edison.brito@comuniclick.com.br>
2016-07-11 09:55:59 -0300

    ajuste teste

--- GetPreviousandNextRow.sql
+++ GetPreviousandNextRow.sql
@@ -1,10 +1,10 @@
-DECLARE @temp TABLE (id int, name varchar(50))  
+﻿DECLARE @temp TABLE (id int, name varchar(50))  
 --Insert Data in table  
 INSERT INTO @temp(id, name)  
 values(1,'João'),(5, 'Maria'),(10,'Jose'),(13,'Filipe'),(16,'Antonio'),(24,'Marcos')  
 DECLARE @ptopicid INT  
 SET @ptopicid = 16  
--- Get Table Records  
+-- Get Table Records   
 SELECT * FROM @temp  
 -- Get Previous and Next Record Ids  
 SELECT (SELECT MAX(t.id) PreviousID  
